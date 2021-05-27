991300a4c21af9c8eba527efb17fe8f7cafed6f1
Vlad Zinevich <vladisznv@gmail.com>
2021-05-27 20:33:31 +0300

    Initial Commit

---
+++ ResumeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResumeApp
+//
+//  Created by Vlad Zinevich on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
